318af3d0f7496d93065f1ab5997d7a4920091554
MLHamadouche <mham@roe.ac.uk>
2020-04-05 14:19:30 +0100

    moved stuff out of func

--- phot_filts.py
+++ phot_filts.py
@@ -6,33 +6,37 @@ from scipy.integrate import simps
 from astropy.cosmology import FlatLambdaCDM
 import spec_fluxes as sf
 
+filters = ["CH2", "HAWKI_K","ISAAC_Ks","CH1","VIMOS_U","f098m","f105w","f125w","f160w", "f435w","f606w", "f775w","f814w", "f850lp"]
+
+filter_curves = []
+
+for filter in filters:
+    filter_curves.append(np.loadtxt("filters/"+str(filter)))
+
+new_filter_curves = []
+
+eff_wavs = []
+
+for f in filter_curves:
+    flux_filter = f[:,1]/np.max(f[:,1])
+    wav_filter = f[:,0]
+    eff_wavs.append(np.sum(wav_filter*flux_filter)/np.sum(flux_filter))
+
+
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+Mpc_m = 3.086*10**22 #Mpc in m
+Mpc_cm = Mpc_m*10**2 #in centimetres
+
 def photometry(waves, fluxes, redshift):
 
-    filters = ["CH2", "HAWKI_K","ISAAC_Ks","CH1","VIMOS_U","f098m","f105w","f125w","f160w", "f435w","f606w", "f775w","f814w", "f850lp"]
-
-    filter_curves = []
-
-    for filter in filters:
-        filter_curves.append(np.loadtxt("filters/"+str(filter)))
-
-    new_filter_curves = []
-
-    eff_wavs = []
-
     for f in filter_curves:
-        flux_filter = f[:,1]/np.max(f[:,1])
-        wav_filter = f[:,0]
-        eff_wavs.append(np.sum(wav_filter*flux_filter)/np.sum(flux_filter))
         filter_interpol = np.interp(waves*(1 + redshift), wav_filter, flux_filter, left=0, right=0) # shift Wavelengths by factor of redshift to the left to get lambda_obs
         new_filter_curves.append(filter_interpol)
 
-    c = 2.99792458*10**10 #cms/s cgs units
+    #c = 2.99792458*10**10 #cms/s cgs units
     #fluxes*=(3.826*10**33)#to get in ergs/s/Angstrom
     fluxes*=(3.826*10**33)
     # for conversion to ergs/s/cm^2/Angstrom
-    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-    Mpc_m = 3.086*10**22 #Mpc in m
-    Mpc_cm = Mpc_m*10**2 #in centimetres
     l_dist=cosmo.luminosity_distance(redshift).value*Mpc_cm #converting Mpc to cm
     lum_area = 4 *np.pi*((l_dist)**2)
     #print("l_dist=", l_dist)
@@ -43,7 +47,7 @@ def photometry(waves, fluxes, redshift):
     #print("new fluxes", new_fluxes)
     # model photometry
     for m in range(14):
-        
+
         f_lam_x_filters = f_lam*new_filter_curves[m]*waves
         new_fluxes[m] = (np.trapz(f_lam_x_filters, x=waves))/(np.trapz(new_filter_curves[m]*waves, x=waves))
 
