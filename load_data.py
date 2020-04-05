019040aff56c6f3caf84e7fec319b1d12c9cbb20
MLHamadouche <mham@roe.ac.uk>
2020-04-05 14:19:03 +0100

    ross objects

--- load_data.py
+++ load_data.py
@@ -7,22 +7,18 @@ import matplotlib.pyplot as plt
 #data_array = np.arange(10)
 flux_cols = ['CH2_flux', 'Ks_HAWKI_flux','Ks_ISAAC_flux','CH1_flux','VIMOS_U_flux','F098M_flux','F105W_flux','F125W_flux','F160W_flux','F435W_flux','F606W_flux','F775W_flux','F814W_flux','F850LP_flux']
 flux_errs_cols = ['CH2_err', 'Ks_HAWKI_err','Ks_ISAAC_err','CH1_err','VIMOS_U_err','F098M_err','F105W_err','F125W_err','F160W_err', 'F435W_err','F606W_err', 'F775W_err','F814W_err', 'F850LP_err']
-catalog_file = Table.read('/Users/massissiliahamadouche/Downloads/VANDELS_CDFS_HST_PHOT_v1.0.fits').to_pandas()
+#catalog_file = Table.read('/Users/massissiliahamadouche/Downloads/VANDELS_CDFS_HST_PHOT_v1.0.fits').to_pandas()
+catalog_file = Table.read('/Users/massissiliahamadouche/Downloads/massi_cdfs_vandels_test_phot.fits').to_pandas()
+
+
+#print(data_array)
+
 
 def load_catalog_data(data_array):
     catalog_IDs = catalog_file['ID']
-    catalog_IDs.index = 'CDFS' + catalog_file['ID'].astype(str).str.pad(6, side="left", fillchar="0") + catalog_file['CAT'].str.decode("utf-8")
+    catalog_IDs.index = 'CDFS' + catalog_file['ID'].astype(str).str.pad(6, side="left", fillchar="0") #+ catalog_file['CAT'].str.decode("utf-8")
     _cat = catalog_IDs[data_array]
-    #print(_cat)
-    #print(catalog_file.index[0])
 
-    #print("CDFS"+table["ID"].astype(str).str.pad(6, side="left", fillchar="0")+table["CAT"].astype(str))
-    #catalog_file.index = catalog_file["ID"]
-    #print(f'catalog_file.index: {catalog_file.index}')
-    #catalog_file.index = catalog_file['ID']
-
-    #_cat= catalog_file.index
-    #print(_cat)
 
     cat_data = catalog_file.loc[_cat, flux_cols].values
     cat_errs = catalog_file.loc[_cat, flux_errs_cols].values
